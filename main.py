85b054b9fc903ee771dc50c2bdc85bbee309f292
ShamHi <shomakakdela@gmail.com>
2023-12-01 18:25:52 +0300

    Add middlewares

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from aiogram import Bot, Dispatcher
 
 from app.routers import inline_router, private_router
+from app.middlewares import InfoLoggerMiddleware, ThrottlingMiddleware
 from app.config import config
 from app import utils
 
 
 def setup_logging(dp: Dispatcher):
     dp["aiogram_logger"] = utils.logging.setup_logger().bind(type="aiogram")
+    dp["throttling_logger"] = utils.logging.setup_logger().bind(type="throttling")
 
 
 def setup_handlers(dp: Dispatcher):
     dp.include_routers(inline_router, private_router)
+
+
+def setup_middlewares(dp: Dispatcher):
+    dp.update.outer_middleware(InfoLoggerMiddleware(logger=dp["aiogram_logger"]))
+    dp.message.middleware(ThrottlingMiddleware(logger=dp["throttling_logger"], throttling_time=5))
 
 
 async def setup_aiogram(dp: Dispatcher, bot: Bot):
